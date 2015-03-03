3e3e83d3d52b0c89a305e49aba504fdb1355ca76
vrajesh <vrajeshs@cloudzon.com>
2015-03-03 13:52:46 +0530

    New DTO for EditEmployee Fields

---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/EditEmployeeDTO.java
@@ -0,0 +1,98 @@
+package com.cloudzon.huddle.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class EditEmployeeDTO {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String retypePassword;
+	private String mobileNumber;
+	private String email;
+	private Date dob;
+	private Date joiningDate;
+	private List<RoleDTO> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRetypePassword() {
+		return retypePassword;
+	}
+
+	public void setRetypePassword(String retypePassword) {
+		this.retypePassword = retypePassword;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+	public List<RoleDTO> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<RoleDTO> roles) {
+		this.roles = roles;
+	}
+
+}
