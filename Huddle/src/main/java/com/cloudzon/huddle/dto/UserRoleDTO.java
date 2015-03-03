473081e7c5ad67fae5428f58ba445acd371dcd07
vrajesh <vrajeshs@cloudzon.com>
2015-03-03 14:16:47 +0530

    DTO for User Role

---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/UserRoleDTO.java
@@ -0,0 +1,30 @@
+package com.cloudzon.huddle.dto;
+
+public class UserRoleDTO {
+	
+	private Long id;
+	private String roleName;
+	
+	public UserRoleDTO(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+}
