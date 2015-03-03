832b2c946c090f0ee9415f59a3cf2355df8b1318
vrajesh <vrajeshs@cloudzon.com>
2015-03-03 13:55:17 +0530

    Added new field : JoinigDate, ProfilePic

--- Huddle/src/main/java/com/cloudzon/huddle/dto/SignupUser.java
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/SignupUser.java
@@ -1,5 +1,8 @@
 package com.cloudzon.huddle.dto;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 
@@ -12,8 +15,7 @@ import com.cloudzon.huddle.constraint.UserExist;
 //@PasswordMatch(password = "password", repassword = "retypePassword", property = "retypePassword", message = "Password Not Match", groups = {
 @PasswordMatch(password = "password", passwordproperty = "password", repassword = "retypePassword", repasswordproperty = "retypePassword", message = "Password Not Match", emptyPasswordMessage = "password must not empty", emptyRepasswordMessage = "retype password must not empty", max = 40, min = 8, passwordLengthMessage = "Password must be 8 to 40 char long", groups = {
 		SignupUser.MvcSignUpUser.class, SignupUser.RestSignUpUser.class })
-@UserExist(email = "email", emailEmptyError = "{NotEmpty.userMaster.email}", invalidEmailError = "Not Valid Email", emailExistError = "Email Already Exist!", emailProperty = "email", userName = "userName", userNameEmptyError = "{NotEmpty.userMaster.userName}", userNameExistError = "UserName Already Exist!", userNameProperty = "userName", groups = {
-		SignupUser.MvcSignUpUser.class, SignupUser.RestSignUpUser.class })
+@UserExist(email = "email", emailEmptyError = "{NotEmpty.userMaster.email}", invalidEmailError = "Not Valid Email", emailExistError = "Email Already Exist!", emailProperty = "email", userName = "userName", userNameEmptyError = "{NotEmpty.userMaster.userName}", userNameExistError = "UserName Already Exist!", userNameProperty = "userName", groups = {SignupUser.MvcSignUpUser.class, SignupUser.RestSignUpUser.class })
 public class SignupUser {
 
 	public interface RestSignUpUser {
@@ -21,6 +23,10 @@ public class SignupUser {
 
 	public interface MvcSignUpUser {
 	}
+	private Long id;
+	private String firstName;
+
+	private String lastName;
 
 	// @NotEmpty(message = "{NotEmpty.userMaster.userName}", groups = {
 	// RestSignUpUser.class, MvcSignUpUser.class })
@@ -30,7 +36,9 @@ public class SignupUser {
 	// RestSignUpUser.class, MvcSignUpUser.class })
 	// @Email(message = "{Email}")
 	private String email;
-
+	
+	private String mobileNumber;
+	
 	// @NotEmpty(message = "{NotEmpty.userMaster.password}", groups = {
 	// RestSignUpUser.class, MvcSignUpUser.class })
 	// @Size(min = 8, max = 40, groups = { RestSignUpUser.class,
@@ -43,19 +51,55 @@ public class SignupUser {
 
 	// @AssertTrue(message = "{NotEmpty.userMaster.termAgreement}")
 	// private Boolean termAgree;
+	private String dob;
 
+	private String joiningDate;
+
+	private String profilePic;
+	
 	@NotEmpty(message = "Please enter redirect url after user activation", groups = { RestSignUpUser.class })
 	@URL(message = "Not valid url", groups = { RestSignUpUser.class })
 	private String redirectURL;
+	
+	private List<Long> rolesId;
 
-	@AssertTrue(message = "Please aggree terms and conditions", groups = {
-			RestSignUpUser.class, MvcSignUpUser.class })
-	@NotNull(message = "Please aggree terms and conditions", groups = {
-			RestSignUpUser.class, MvcSignUpUser.class })
-	private Boolean isAgree;
+	/*
+	 * @AssertTrue(message = "Please aggree terms and conditions", groups = {
+	 * RestSignUpUser.class, MvcSignUpUser.class })
+	 * 
+	 * @NotNull(message = "Please aggree terms and conditions", groups = {
+	 * RestSignUpUser.class, MvcSignUpUser.class })
+	 */
+	// private Boolean isAgree;
+	
+	
 
 	public String getUserName() {
 		return userName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public void setUserName(String userName) {
@@ -68,6 +112,14 @@ public class SignupUser {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
 	}
 
 	public String getPassword() {
@@ -97,16 +149,47 @@ public class SignupUser {
 		return redirectURL;
 	}
 
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getJoiningDate() {
+		return joiningDate;
+	}
+
+	public String getProfilePic() {
+		return profilePic;
+	}
+
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+
+	public void setJoiningDate(String joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
 	public void setRedirectURL(String redirectURL) {
 		this.redirectURL = redirectURL;
 	}
 
-	public Boolean getIsAgree() {
-		return isAgree;
+	public List<Long> getRolesId() {
+		return rolesId;
 	}
 
-	public void setIsAgree(Boolean isAgree) {
-		this.isAgree = isAgree;
+	public void setRolesId(List<Long> rolesId) {
+		this.rolesId = rolesId;
 	}
+
+	
+	/*
+	 * public Boolean getIsAgree() { return isAgree; }
+	 * 
+	 * public void setIsAgree(Boolean isAgree) { this.isAgree = isAgree; }
+	 */
 
 }
