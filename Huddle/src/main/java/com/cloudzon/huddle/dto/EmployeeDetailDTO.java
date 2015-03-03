b86e134a8aef64b903faad567dba7a1fb053d692
vrajesh <vrajeshs@cloudzon.com>
2015-03-03 13:54:03 +0530

    New DTO for Get Employee List

---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/EmployeeDetailDTO.java
@@ -0,0 +1,78 @@
+package com.cloudzon.huddle.dto;
+
+import java.util.Date;
+
+public class EmployeeDetailDTO {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Date dob;
+	private Date joiningDate;
+
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+	public EmployeeDetailDTO(Long id, String firstName, String lastName,
+			String email, Date dob, Date joiningDate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.dob = dob;
+		this.joiningDate = joiningDate;
+	}
+
+	
+
+	
+
+}
