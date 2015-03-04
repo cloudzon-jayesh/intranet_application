241173cbd85548bdbe3263978661fbf3d5da5ee1
jayesh <jayesh@cloudzon.com>
2015-03-04 11:07:12 +0530

    add new query for get permission

--- Huddle/src/main/java/com/cloudzon/huddle/repository/PermissionRepository.java
+++ Huddle/src/main/java/com/cloudzon/huddle/repository/PermissionRepository.java
@@ -3,12 +3,20 @@ package com.cloudzon.huddle.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.cloudzon.huddle.dto.GroupPermissionDTO;
 import com.cloudzon.huddle.model.Permission;
+import com.cloudzon.huddle.model.Role;
 
 public interface PermissionRepository extends BaseRepository<Permission> {
 
 	@Query(value = "SELECT permission FROM Permission AS permission")
 	public List<Permission> getAllPermission();
+	
+	@Query(value = "SELECT NEW com.cloudzon.huddle.dto.GroupPermissionDTO(permission.id,permission.permission) FROM Permission AS permission")
+	public List<GroupPermissionDTO> getAllPermissionName();
 
+	@Query("SELECT permission FROM Permission AS permission WHERE permission.id=:permissionId")
+	public Permission getPermissionById(@Param("permissionId") Long permissionId);
 }
