d3712f84deae7a09c4026fba177d2eb7571dc3e4
jayesh <jayesh@cloudzon.com>
2015-03-11 10:09:13 +0530

    backbone view for set activity permission

--- Huddle/src/main/webapp/resources/js/view/PermissionView.js
+++ Huddle/src/main/webapp/resources/js/view/PermissionView.js
@@ -3,51 +3,29 @@ var PermissionView = Backbone.View
 			
 			el : $("#main-container"),
 			initialize : function() {
+			},
+			render : function() {
 				var r_perm="";
 				var r_val;
 				var w_perm="";
 				var w_val;
 				var d_perm="";
 				var d_val;
-			},
-			render : function() {
 				var template = _.template($("#group_template").html(), {});
 				this.$el.html(template);
-				this.getGroupPermission();
 				this.getGroupData();
+				this.getAvtivityData();
 			},
-			events : {
-				"click #add_button" : "reset"
-			},
-			getGroupPermission : function()
+			events :
 			{
-				
-						$.ajax({
-							url : 'rest/user/getGroupPermission.json',
-							type : 'GET',
-							success : function(data)
-							{
-								var len = data.length;
-								for (var i = 0; i < len; i++) 
-								{
-									if(data[i].permission === "R")
-									{
-										r_val = data[i].id;
-										r_perm = data[i].permission;
-									}
-									else if(data[i].permission === "W")
-									{
-										w_val = data[i].id;
-										w_perm = data[i].permission;
-									}
-									else if(data[i].permission === "D")
-									{
-										d_val = data[i].id;
-										d_perm = data[i].permission;
-									}
-								}
-							}
-						});
+				"click #roles" : "reset"
+			},
+			reset : function()
+			{
+				$("input:checkbox[name^='group']").each(function() {
+					$(this).attr('checked',false);
+				});
+				$('#msgBox').html("");
 			},
 			getGroupData : function()
 			{
@@ -57,93 +35,161 @@ var PermissionView = Backbone.View
 					success : function(data)
 					{
 						var len = data.length;
-						var chk = "";
+						var select = $("<select id='roles' style='width : 250px;'></selcet>");
+						for (var i = 0; i < len; i++) 
+						{
+							select.append("<option value="+data[i].id+">" + data[i].roleName + "</option>");
+						}
+						$("#group_data").append(select);
+					
+					}
+				});	
+			
+			},
+			getAvtivityData : function()
+			{
+				$.ajax({
+					url : 'rest/user/getGroupPermission.json',
+					type : 'GET',
+					success : function(data)
+					{
+						var len = data.length;
+						for (var i = 0; i < len; i++) 
+						{
+							if(data[i].permission === "R")
+							{
+								r_val = data[i].id;
+								r_perm = data[i].permission;
+							}
+							else if(data[i].permission === "W")
+							{
+								w_val = data[i].id;
+								w_perm = data[i].permission;
+							}
+							else if(data[i].permission === "D")
+							{
+								d_val = data[i].id;
+								d_perm = data[i].permission;
+							}
+						}
+					}
+				});
+				
+				$.ajax({
+					url : 'rest/user/getActivity.json',
+					type : 'GET',
+					success : function(data)
+					{
 						var thead = $("<thead></thead>");
-						var trh = $("<tr></tr>");
-						var th0 = $("<th>No</th>");
-						var th1 = $("<th>Group Name</th>");
-						var th2 = $("<th>Permission</th>");
-						var th3 = $("<th>Add</th>");
+						var trh = $("<tr style='border: 1px solid #ccc; text-align: center;'></tr>");
+						var th0 = $("<th style='border: 1px solid #ccc; text-align: center;'>No</th>");
+						var th1 = $("<th style='border: 1px solid #ccc; text-align: center;'> Screen Name</th>");
+						var th2 = $("<th style='border: 1px solid #ccc; text-align: center;'>Permission</th>");
 						trh.append(th0);
 						trh.append(th1);
 						trh.append(th2);
-						trh.append(th3);
-						thead.append(trh);
-						$("#group_data").append(thead);
+						thead.append(trh); 
+						$("#event_data").append(thead);
+						var len = data.length;
 						var tBody = $("<tbody></tbody>");
 						for (var i = 0; i < len; i++) 
 						{
-							var tr = $("<tr></tr>");
-							var td0 = $("<td>" + data[i].id	+ "</td>");
-							var td1 = $("<td>" + data[i].roleName + "</td>");
-							var td2 = $("<td>" +
-									"<input type='checkbox' value="+r_val+">&nbsp;"+r_perm+
-									"&nbsp;<input type='checkbox' value="+w_val+">&nbsp;"+w_perm+
-									"&nbsp;<input type='checkbox' value="+d_val+">&nbsp;"+d_perm
+							var tr = $("<tr class='trGroup' style='border: 1px solid #ccc; text-align: center;'></tr>");
+							var td0 = $("<td style='border: 1px solid #ccc; text-align: center;'>" + (i+1)	+ "</td>");
+							var td1 = $("<td style='border: 1px solid #ccc; text-align: center;'>"+data[i].activityName+"</td>");
+							var td2 = $("<td id='list' style='border: 1px solid #ccc; text-align: center;'>" +
+									"<input type='checkbox'  name='group'  value="+r_val+" attr-name='"+ data[i].id + "'>&nbsp;"+r_perm+
+									"&nbsp;<input type='checkbox' name='group'  value="+w_val+" attr-name='"+ data[i].id + "'>&nbsp;"+w_perm+
+									"&nbsp;<input type='checkbox' name='group'  value="+d_val+" attr-name='"+ data[i].id + "'>&nbsp;"+d_perm
 									+"</td>");
-							var td3 = $("<td></td>");
-							var button = $("<input type='button' value='Set Permission' class='add-btn' attr-name='"+ data[i].id + "'>");
-							td3.append(button);
 							tr.append(td0);
 							tr.append(td1);
 							tr.append(td2);
-							tr.append(td3);
 							tBody.append(tr);
-							
-							$("#group_data").append(tBody);
+							$("#event_data").append(tBody);
 						}
-						$('.add-btn').click(function(i) {
-							var name = $(this).attr("attr-name");
-							var val = [];
-							 $(':checkbox:checked').each(function(i){
-						          val[i] = $(this).val();
-						        });
-							alert(val);
-							console.log(name);
+						var trl = $("<tr></tr>");
+						var tdd = $("<td colspan='3' style='border: 1px solid #ccc; text-align: center;'></td>");
+						var button = $("<input value='Set Permission' class='add-btn' id='addButton' type='Button'>");
+						var sp = $("<span id='msgBox' style='color:#008CBA;'></span>");
+						tdd.append(button);
+						tdd.append(sp);
+						trl.append(tdd);
+						$("#event_data").append(trl);
+						
+						$('#roles').change(function(){
 							$.ajax({
-								url : 'rest/user/addGroupPermission.json',
-								type : 'POST',
-								data : JSON.stringify(
-									{	
-										"id" : name,
-										"permissionId" : val
-									}),
-									headers : 
+								url : 'rest/user/getPermissionRoleActivity.json',
+								type: 'GET',
+								success : function(data)
+								{
+									var l = data.length;
+									var values;
+									for(var i=0;i<l;i++)
 									{
-										'Accept' : 'application/json',
-										'Content-Type' : 'application/json; charset=UTF-8'
-									},
-									success :function(data)
+										group = $("#roles option:selected").val();
+											$('.trGroup').each(function()
+											{
+												values = data[i].permissionId;
+												var getval;
+												var name;
+												$("input:checkbox[name^='group']").each(function() 
+												{
+													getval = $(this).val();
+													name = $(this).attr("attr-name");
+													if (getval == values && group == data[i].roleId  && name == data[i].activityId) 
+													{
+														$(this).prop('checked',true);
+														//console.log("per : " + getval +"    , grp : " + group +"act  :  "+name);
+														//($('#list [value="'+values+'"]').prop('checked',true));
+													}
+												});
+											});
+										}
+								},
+								error : function()
+								{
+									console.log('error');
+								}
+								
+							});
+						});
+						
+						var permissionModel = new PermissionModel();
+						$('#addButton').click(function() 
+						{
+							group = $("#roles option:selected").val();
+							$('.trGroup').each(function(i)
+							{
+								var val = [];
+								var name = "";
+								$(this).find(':checkbox:checked').each(function(j)
+								{
+									name = $(this).attr("attr-name");
+									val[j] = $(this).val();
+								});
+								this.permissionModel = new PermissionModel
+								({
+									  "groupId" : group, 
+					        		  "activityId" : name,
+					        		  "permissionIds" : val
+								});
+								this.permissionModel.save({}, 
+								{
+									success: function(response) 
 									{
-										console.log("Success");
+										//$('#try').css('background-color:#E0ECF1; color:#fff;')
+										$('#msgBox').html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &quot; Permission is set &quot;");
+										console.log("1 record inserted");
 									},
-									error : function(data) {
-										console.log("Success");
+									error : function(data)
+									{
+										console.log("error");
 									}
+								});
 							});
 						});
 					}
-				});
-						
-			},
-			reset : function()
-			{
-				$.ajax({
-					url : 'rest/user/addGroup.json',
-					type : 'POST',
-					data : JSON.stringify({	"roleName" : $("#groupName").val()}),
-					headers : 
-						{
-							'Accept' : 'application/json',
-							'Content-Type' : 'application/json; charset=UTF-8'
-						},
-					success :function(data)
-					{
-						console.log("Success");
-					},
-					error : function(data) {
-						console.log("Success");
-					}
-				});
-			}
+				});	
+			}								
 });
