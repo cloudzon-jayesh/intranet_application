c488cb4d847e1d38f978703282220cc86e3aa9fc
jayesh <jayesh@cloudzon.com>
2015-03-31 16:32:00 +0530

    DTO for user permission

---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/SetUserPermissionDTO.java
@@ -0,0 +1,44 @@
+package com.cloudzon.huddle.dto;
+
+import java.util.List;
+
+public class SetUserPermissionDTO 
+{
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private List<Long> roleIds;
+	private List<RoleActivityPermissionDTO> roleActivityPermissionDTOs;	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public List<Long> getRoleIds() {
+		return roleIds;
+	}
+	public void setRoleIds(List<Long> roleIds) {
+		this.roleIds = roleIds;
+	}
+	public List<RoleActivityPermissionDTO> getRoleActivityPermissionDTOs() {
+		return roleActivityPermissionDTOs;
+	}
+	public void setRoleActivityPermissionDTOs(
+			List<RoleActivityPermissionDTO> roleActivityPermissionDTOs) {
+		this.roleActivityPermissionDTOs = roleActivityPermissionDTOs;
+	}
+	
+}
