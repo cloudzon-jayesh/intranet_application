a81ac734eef18db502ccc9667a7574ac3e368149
jayesh <jayesh@cloudzon.com>
2015-03-31 16:28:40 +0530

    DTOs for Activity

--- Huddle/src/main/java/com/cloudzon/huddle/dto/ActivityDTO.java
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/ActivityDTO.java
@@ -4,24 +4,50 @@ public class ActivityDTO
 {
 	private Long id;
 	private String activityName;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getActivityName() {
-		return activityName;
-	}
-	public void setActivityName(String activityName) {
-		this.activityName = activityName;
-	}
-	public ActivityDTO(Long id, String activityName) {
+	private String activityLink;
+	
+	
+	public ActivityDTO(Long id, String activityName, String activityLink) {
 		super();
 		this.id = id;
 		this.activityName = activityName;
+		this.activityLink = activityLink;
 	}
-	
-	
 
+
+	public ActivityDTO(Long id, String activityLink) {
+		super();
+		this.id = id;
+		this.activityLink = activityLink;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getActivityName() {
+		return activityName;
+	}
+
+
+	public void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
+
+
+	public String getActivityLink() {
+		return activityLink;
+	}
+
+
+	public void setActivityLink(String activityLink) {
+		this.activityLink = activityLink;
+	}
 }
--- Huddle/src/main/java/com/cloudzon/huddle/dto/ActivityRolePermissionDTO.java
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/ActivityRolePermissionDTO.java
@@ -7,23 +7,26 @@ public class ActivityRolePermissionDTO
 	private Long groupId;
 	private Long activityId;
 	private List<Long> permissionIds;
-	
 	public Long getGroupId() {
 		return groupId;
 	}
 	public void setGroupId(Long groupId) {
 		this.groupId = groupId;
 	}
+	
 	public Long getActivityId() {
 		return activityId;
 	}
 	public void setActivityId(Long activityId) {
 		this.activityId = activityId;
 	}
+	
 	public List<Long> getPermissionIds() {
 		return permissionIds;
 	}
 	public void setPermissionIds(List<Long> permissionIds) {
 		this.permissionIds = permissionIds;
 	}
+	
+	
 }
