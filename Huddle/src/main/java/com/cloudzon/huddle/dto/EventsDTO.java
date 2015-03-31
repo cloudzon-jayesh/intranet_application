0bdf8771ed2dd5ee556a347257596b1f0a71ccb1
jayesh <jayesh@cloudzon.com>
2015-03-31 16:29:46 +0530

    DTOs for Events

---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/EventsDTO.java
@@ -0,0 +1,43 @@
+package com.cloudzon.huddle.dto;
+
+
+public class EventsDTO 
+{
+	private Long id;
+	private String eventName;
+	private String description;
+	private String date;
+	private String time;
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
---
+++ Huddle/src/main/java/com/cloudzon/huddle/dto/EventsListDTO.java
@@ -0,0 +1,54 @@
+package com.cloudzon.huddle.dto;
+
+import java.util.Date;
+
+public class EventsListDTO 
+{
+	private Long id;
+	private String eventName;
+	private String description;
+	private Date date;
+	private Date time;
+	public EventsListDTO(Long id, String eventName, String description,
+			Date date, Date time) {
+		super();
+		this.id = id;
+		this.eventName = eventName;
+		this.description = description;
+		this.date = date;
+		this.time = time;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	
+	
+
+}
